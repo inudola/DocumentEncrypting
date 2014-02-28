1835fb9cf3f24a6289bbf075431cfc762e45fcc1
dmitriyTarasov <crash_pk@inbox.ru>
2014-02-28 23:14:01 +0800

    Add comments for ICrypto interface

--- src/io/docencrypting/Crypto/ICrypto.java
+++ src/io/docencrypting/Crypto/ICrypto.java
@@ -1,13 +1,28 @@
 package io.docencrypting.Crypto;
 
 import io.docencrypting.Entities.CryptoEntity;
-
 import java.io.IOException;
+
+/**
+ * General interface for all cipher algorithms
+ */
 
 public interface ICrypto {
 
+    /**
+     * Decode the file that provide {@link io.docencrypting.Entities.CryptoEntity}
+     * @param entity contains all information that needed for cipher algorithm
+     * @throws IOException
+     * @see #decode(io.docencrypting.Entities.CryptoEntity)
+     */
     public void decode(CryptoEntity entity) throws IOException;
 
+    /**
+     * Encode the file that provide {@link io.docencrypting.Entities.CryptoEntity}
+     * @param entity contains all information that needed for cipher algorithm
+     * @throws IOException
+     * @see #decode(io.docencrypting.Entities.CryptoEntity)
+     */
     public void encode(CryptoEntity entity) throws IOException;
 
 }
