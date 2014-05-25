eb2f46feb8835d366d0bac3bc8afaa7007898472
Denis Khomusyak <khomusyak.prog@ya.ru>
2014-05-25 22:47:32 +0800

    Logging

--- src/io/docencrypting/UI/Graphical/SettingsFrame.java
+++ src/io/docencrypting/UI/Graphical/SettingsFrame.java
@@ -1,18 +1,17 @@
 package io.docencrypting.UI.Graphical;
 
-import io.docencrypting.Config.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger; // обратил внимание?
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
 
 public class SettingsFrame extends JFrame {
 
-    private JCheckBox needHiddenCheck = new JCheckBox();
     private JCheckBox replaceExistsFileCheck = new JCheckBox();
     private JTextField postfixField = new JTextField("_postfix");
     private JTextField widthScreenField = new JTextField();
@@ -20,34 +19,48 @@ public class SettingsFrame extends JFrame {
     private JButton save = new JButton("Save");
     private JButton cancel = new JButton("Cancel");
 
-    private JLabel needHidden = new JLabel("Hide files");
     private JLabel replace = new JLabel("Replace exists file");
     private JLabel postfix = new JLabel("Postfix:");
     private JLabel x = new JLabel("x");
     private JLabel screenSize = new JLabel("Screen size:");
 
-    private static Log log = new Log();
+    //private static Log log = new Log();
+    private static final Logger logger = LogManager.getLogger(SettingsFrame.class);
 
     Box boxSettings = new Box(BoxLayout.Y_AXIS);
 
     public SettingsFrame() {
 
-        boxSettings.add(BoxCreator.makeLineBox(needHidden, needHiddenCheck));
         boxSettings.add(BoxCreator.makeLineBox(replace, replaceExistsFileCheck, postfix, postfixField));
         boxSettings.add(BoxCreator.makeLineBox(screenSize, widthScreenField, x, heightScreenField));
         boxSettings.add(BoxCreator.makeLineBox(save, cancel));
 
+
+
         save.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                logger.debug("Start processing");
+                try {
+                    if(widthScreenField.getText().equals("") && heightScreenField.getText().equals(""))
+                    {
+                        setScreenSize(widthScreenField.getText(), heightScreenField.getText());
+                    }
+                    if(replaceExistsFileCheck.isSelected())
+                    {
+                        if(postfixField.getText().equals(""))
+                        {
 
-                //if(wi)
-
-//                setScreenSize(widthScreenField., heightScreenField);
-//                JOptionPane.showMessageDialog(null, "Success!");
+                        }
+                    }
+                } catch (Exception ex) {
+                    logger.error("Something failed", e);
+                }
+                JOptionPane.showMessageDialog(null, "Success!");
+                logger.debug("done");
             }
         });
-
+//это удалить надо
         cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -58,7 +71,7 @@ public class SettingsFrame extends JFrame {
 
         centerFrame();
         setContentPane(boxSettings);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setVisible(true);
     }
 
@@ -68,7 +81,7 @@ public class SettingsFrame extends JFrame {
         setLocationRelativeTo(null);
     }
 
-    public static void  setScreenSize(int width, int height){
+    public static void  setScreenSize(String width, String height){
         File filesettings = new File("settings.txt");
         //log.write("string");
 
